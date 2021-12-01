e9e0cb8f5088f6c6919bb156ccf49c5f407e4745
smkisvarday <susannahkisvarday@hotmail.com>
2021-11-30 19:13:11 -0500

    commit1

---
+++ LoopOlympics.js
@@ -0,0 +1,12 @@
+for( var i =0; i , 10; i++) {
+    console.log (i);
+}
+
+for (var i = 9; i >= 0; i--) {
+    console.log (i);
+}
+
+var fruit = ["banana", "orange", "apple", "kiwi"]
+for (var i = 0; i < fruit.length; i++) {
+    console.log (fruit[i]);
+}
